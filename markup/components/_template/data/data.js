ac17d065e3706fb27f7008bbe62c9eec59790a28
pashkes <pashkes@gmail.com>
2018-06-26 10:46:44 +0300

    first commit

---
+++ markup/components/_template/data/data.js
@@ -0,0 +1,21 @@
+/* Module data structure */
+
+// moduleName: {
+//     dataType: {
+//         property: value
+//     }
+// }
+
+/* Module data example */
+
+_template: {
+  big: {
+    title: 'Hello world',
+      age
+  :
+    10,
+      button
+  :
+    false
+  }
+}

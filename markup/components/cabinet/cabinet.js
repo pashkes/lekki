667e3e71b88d6a401c8b924f2e4e0dc59ff783a0
pashkes <pashkes@gmail.com>
2018-08-08 15:17:19 +0300

    fix

--- markup/components/cabinet/cabinet.js
+++ markup/components/cabinet/cabinet.js
@@ -9,19 +9,8 @@
     placeholder: 'Месяц',
     floatWidth: 144
   });
-  $('.js-custom-select-year').SumoSelect({
-    nativeOnDevice: ['Android', 'BlackBerry', 'iPhone', 'iPad', 'iPod', 'Opera Mini', 'IEMobile', 'Silk'],
-    placeholder: 'Год',
-    floatWidth: 144
-  });
-  if ($('.cabinet .options').length) {
-    new SimpleBar($('.cabinet .options')[0],
-      {autoHide: false});
-    new SimpleBar($('.cabinet .options')[1],
-      {autoHide: false});
-    new SimpleBar($('.cabinet .options')[2],
-      {autoHide: false});
-  }
+
+
   $('.js-show-hide-password').on('click', function () {
     $(this).toggleClass('active');
     if ($(this).prev().attr('type') === 'password') {
@@ -31,3 +20,27 @@
     }
   })
 })();
+var selectYear;
+$(document).ready(function () {
+  selectYear = $('.js-custom-select-year').SumoSelect({
+    nativeOnDevice: ['Android', 'BlackBerry', 'iPhone', 'iPad', 'iPod', 'Opera Mini', 'IEMobile', 'Silk'],
+    placeholder: 'Год',
+    floatWidth: 144
+  });
+  var yearScroll;
+  if ($('.cabinet .options').length) {
+    new SimpleBar($('.cabinet .options')[0],
+      {autoHide: false});
+    new SimpleBar($('.cabinet .options')[1],
+      {autoHide: false});
+    yearScroll = new SimpleBar($('.cabinet .options')[2],
+      {autoHide: false});
+  }
+
+  $('.cabinet__select--year').find('.SelectBox').on('click', function () {
+    yearScroll.getScrollElement().scrollTop = 731;
+  });
+
+});
+
+
